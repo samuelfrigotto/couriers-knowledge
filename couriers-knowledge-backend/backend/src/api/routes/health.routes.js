61120d4d5b9d3670cddc2deac1e403d77fd0d964
Samuel <samuelfrigotto@gmail.com>
2025-07-17 12:29:19 -0300

    changes

--- couriers-knowledge-backend/backend/src/api/routes/health.routes.js
+++
@@ -1,9 +0,0 @@
-// backend/src/api/routes/health.routes.js
-const express = require('express');
-const router = express.Router();
-const healthController = require('../../controllers/health.controller');
-
-// A rota agora é /health e chama a nova função getHealthStatus
-router.get('/health', healthController.getHealthStatus);
-
-module.exports = router;
